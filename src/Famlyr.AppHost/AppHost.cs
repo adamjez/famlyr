b60cfe3f63f1f505ab94c89cb9f3c511ce16b84e
Adam Jez <adamj@example.com>
2026-09-19 20:54:14 +0200

    chore: start pg admin on demand

--- src/Famlyr.AppHost/AppHost.cs
+++ src/Famlyr.AppHost/AppHost.cs
@@ -2,7 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("postgres")
     .WithDataVolume()
-    .WithPgAdmin();
+    .WithPgAdmin(b => b.WithExplicitStart());
 
 var database = postgres.AddDatabase("famlyrdb");
 
